9dff2a6798de9445b6e3e4b135aa3dc2c75db3d8
Lokesh <lokeshsingh2182000@gmail.com>
2020-06-23 17:26:15 +0530

    second commit

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
+
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#blah')
+            $('#image')
                 .attr('src', e.target.result)
                 .width(500)
                 .height(500);
         };
-
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-var options =
-[
-  {
-    "text"  : "C++"
-  },
-  {
-    "text"  : "Java"
-  },
-  {
-    "text"  : "Python"
-  },
-  {
-    "text"  : "HTML"
-  },
-  {
-    "text"  : "JavaScript"
-  }
-];
-var selectbox=document.getElementById("selectbox");
-for(var i=0;i<options.length;i++){
-	selectbox.options.add(new Option(options[i].text));
+function addSkill(input){
+  $("#skill").append("<li><span><i class='fa fa-trash'></i></span> "+input.value+"</li>");
+  var mySelect = document.getElementById("selectbox");
+  mySelect.selectedIndex=0;
 }
 
+$("#skill").on("click","span",function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+});
